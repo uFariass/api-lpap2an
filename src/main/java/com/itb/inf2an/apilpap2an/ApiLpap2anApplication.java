f09fe5558c0de828ca1a320b042c49e9d0250646
Lucas Farias dos Santos <rm90326@estudante.fieb.edu.br>
2024-09-24 22:18:07 -0300

    Início Controle de Versão

---
+++ src/main/java/com/itb/inf2an/apilpap2an/ApiLpap2anApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.apilpap2an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLpap2anApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLpap2anApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itb/inf2an/apilpap2an/ApiLpap2anApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.apilpap2an;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLpap2anApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
